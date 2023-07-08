d7ade7199626f623dd625e6227297b57b2ced06d
javierhol <javierholguin596@gmail.com>
2023-07-08 11:40:19 -0500

    NEW: add color theme

--- src/components/ThemeItem.jsx
+++ src/components/ThemeItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ThemeItem = () => {
+const ThemeItem = ({color, img}) => {
   return (
-    <div>ThemeItem</div>
+    <img src={img} alt="" className="theme__img" />
   )
 }
 
--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { themes } from '../data'
 import ThemeItem from './ThemeItem'
 import {FaCog} from 'react-icons/fa'
+import {BsSun,BsMoon} from 'react-icons/bs'
+import './themes.css'
 
 const Themes = () => {
+  const [showSwitcher, setShowSwitcher] = useState(false)
   return (
-    <div>Themes</div>
+    <div>
+    <div className='style__switcher'>
+      <div className="style__switcher-toggle">
+        <FaCog />
+      </div>
+      <div className="theme__toggle">
+        <BsMoon />
+      </div>
+      <h3 className="style__switcher-title">Style Switcher </h3>
+      <div className="style__switcher-items">
+        {themes.map((theme,index)=>{
+          return <ThemeItem key={index} {...theme} />
+        })}
+      </div>
+      <div className="style__switcher-close">
+        &times;
+      </div>
+    </div>  
+  </div>
   )
 }
 
