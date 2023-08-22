97a2161c4089cd635fedab3066688fc2132dd3a9
FractionOfCode <javierholguin596.com>
2023-08-21 19:22:41 -0500

    NEW: Implementacion color and theme

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -5,17 +5,47 @@ import {FaCog} from 'react-icons/fa'
 import {BsSun,BsMoon} from 'react-icons/bs'
 import './themes.css'
 
+const getStorageColor = ()=>{
+  let color = 'hsl(252, 35%, 51%)'
+  if(localStorage.getItem('color')){
+    color = localStorage.getItem('color')
+  }
+  return color
+}
+const getStorageTheme = ()=>{
+  let theme = 'light-theme'
+  if(localStorage.getItem('theme')){
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 const Themes = () => {
   const [showSwitcher, setShowSwitcher] = useState(false)
-  const [color, setColor] = useState('red')
+  const [color, setColor] = useState(getStorageColor())
+  const [theme, setTheme] = useState(getStorageTheme())
 
   const changeColor = (color) => {
     setColor(color)
   }
 
+  const toggleTheme = () => {
+    if(theme === 'light-theme'){
+      setTheme('dark-theme')
+    }else{
+      setTheme('light-theme')
+    }
+  }
+
   useEffect(()=>{
     document.documentElement.style.setProperty('--first-color',color)
+    localStorage.setItem('color',color)
   }, [color])
+
+  useEffect(()=>{
+    document.documentElement.className = theme
+    localStorage.setItem('theme',theme)
+  }, [theme])
 
   return (
     <div>
@@ -25,13 +55,13 @@ const Themes = () => {
       }}>
         <FaCog />
       </div>
-      <div className="theme__toggle">
-        <BsMoon />
+      <div className="theme__toggle" onClick={toggleTheme}>
+        {theme === 'light-theme' ? <BsMoon /> : <BsSun/>}
       </div>
       <h3 className="style__switcher-title">Style Switcher </h3>
       <div className="style__switcher-items">
         {themes.map((theme,index)=>{
-          return <ThemeItem key={index} {...theme} />
+          return <ThemeItem key={index} {...theme} changeColor={changeColor} />
         })}
       </div>
       <div className="style__switcher-close" onClick={()=>{
